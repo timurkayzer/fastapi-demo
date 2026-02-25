d873145f9be1859150b2aa73b5ef834d96ed31d9
Timur Kayzer <timur@Mac.fritz.box>
2026-02-26 00:06:54 +0100

    feat: db, cicd

---
+++ db/engine.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from os import getenv
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+host = getenv("DB_HOST")
+port = getenv("DB_PORT")
+user = getenv("DB_USER")
+password = getenv("DB_PASSWORD")
+database = getenv("DB_NAME")
+
+if not all([host, port, user, password, database]):
+    raise ValueError("Database connection parameters are not fully set in environment variables.")
+
+connection_string = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(connection_string)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
