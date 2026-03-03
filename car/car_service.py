95f6278e9211d0a710e57698422d7fa984f8ec55
Timur Kayzer <timur@Mac.fritz.box>
2026-03-03 23:30:20 +0100

    fix: yml, service

--- car/car_service.py
+++ car/car_service.py
@@ -3,32 +3,34 @@ from car.car_model import CarModel
 
 
 class CarService:
-    def get_by_id(self, car_id):
-        return session.query(CarModel).filter_by(id=car_id).first()
+    async def get_by_id(self, car_id):
+        return await session.query(CarModel).filter_by(id=car_id).first()
 
-    def get_all(self):
-        return session.query(CarModel).all()
+    async def get_all(self):
+        return await session.query(CarModel).all()
     
-    def create(self, car_data):
+    async def create(self, car_data):
         new_car = CarModel(**car_data)
         session.add(new_car)
-        session.commit()
+        await session.commit()
         return new_car
     
-    def update(self, car_id, car_data):
-        car = session.query(CarModel).filter_by(id=car_id).first()
+    async def update(self, car_id, car_data):
+        car = await self.get_by_id(car_id)
         if not car:
             return None
         for key, value in car_data.items():
             setattr(car, key, value)
-        session.commit()
+        await session.commit()
         return car
     
-    def delete(self, car_id):
-        car = session.query(CarModel).filter_by(id=car_id).first()
+    async def delete(self, car_id):
+        car = await self.get_by_id(car_id)
         if not car:
             return False
         session.delete(car)
         session.commit()
         return True
     
+
+car_service = CarService()
