5cb67e1b941d32fbcde8ec4f5a7421ba3813344f
Timur Kayzer <timur@MacBook-Air.local>
2026-03-07 18:52:50 +0100

    fix: app runs finally lol

--- car/car_controller.py
+++ car/car_controller.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, HTTPException
-from .car_model import Car, CarBase
+from .car_model import CarBase
 from .car_service import car_service
 
 router = APIRouter(prefix="/car", tags=["car"])
 
-@router.get("/", response_model=[Car])
+@router.get("/")
 async def get_car():
     # For demonstration, we will return a dummy car. In a real application, you would fetch this from a database.
     return car_service.get_all()
 
-@router.get("/{car_id}", response_model=Car)
+@router.get("/{car_id}")
 async def get_car(car_id: int):
     # For demonstration, we will return a dummy car. In a real application, you would fetch this from a database.
     car = await car_service.get_by_id(car_id)
@@ -18,7 +18,7 @@ async def get_car(car_id: int):
     else:
         return car
     
-@router.post("/", response_model=Car)
+@router.post("/")
 async def create_car(car: CarBase):
     # In a real application, you would save the car to a database and return the saved object.
     return car_service.create(car)
